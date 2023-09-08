858e3acf5d26fa7985b41897f1caf6f80ded62ad
saqibsoit <51706503+saqibsoit@users.noreply.github.com>
2023-09-08 17:28:29 +0500

    Initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="../images/react-icon-small.png" alt='logo' className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  )
+}
+
+export default Navbar
